9dfbf0eb529c2b79b99783dc117688a1907ca4ff
jvican <jorgevc@fastmail.es>
2017-11-16 14:58:20 +0100

    Remove duplication in scala instance and add todo

--- backend/src/main/scala/bloop/ScalaInstance.scala
+++ backend/src/main/scala/bloop/ScalaInstance.scala
@@ -18,29 +18,31 @@ class ScalaInstance(
   override lazy val loader: ClassLoader =
     new URLClassLoader(allJars.map(_.toURI.toURL), null)
 
+  private def isJar(filename: String): Boolean = filename.endsWith(".jar")
+  private def hasScalaCompilerName(filename: String): Boolean =
+    filename.startsWith("scala-compiler-")
+  private def hasScalaLibraryName(filename: String): Boolean =
+    filename.startsWith("scala-library-")
+
   override val compilerJar: File =
-    allJars
-      .find(f => f.getName.startsWith("scala-compiler-") && f.getName.endsWith(".jar"))
-      .orNull
-
+    allJars.find(f => isJar(f.getName) && hasScalaCompilerName(f.getName)).orNull
   override val libraryJar: File =
-    allJars
-      .find(f => f.getName.startsWith("scala-library-") && f.getName.endsWith(".jar"))
-      .orNull
+    allJars.find(f => isJar(f.getName) && hasScalaLibraryName(f.getName)).orNull
+  override val otherJars: Array[File] = allJars.filter { file =>
+    val filename = f.getName
+    isJar(filename) && !hasScalaCompilerName(filename) && !hasScalaLibraryName(filename)
+  }
 
-  override val otherJars: Array[File] =
-    allJars.filter(
-      f =>
-        f.getName.endsWith(".jar") && !(f.getName
-          .startsWith("scala-compiler-") || f.getName.startsWith("scala-library-")))
-
-  override def actualVersion(): String =
+  /** Tells us what the real version of the classloaded scalac compiler in this instance is. */
+  override def actualVersion(): String = {
+    // TODO: Report when the `actualVersion` and the passed in version do not match.
     Option(loader.getResource("compiler.properties")).map { url =>
       val stream     = url.openStream()
       val properties = new Properties()
       properties.load(stream)
       properties.get("version.number").asInstanceOf[String]
     }.orNull
+  }
 }
 
 object ScalaInstance {
