cd5dd223d560a5c5278ef75c3354aed0a76ea73d
Martin Duhem <martin.duhem@gmail.com>
2017-12-03 17:49:18 +0100

    Improve logging abstraction, Hide L4J better
    It was very tedious to define new subclasses of `Logger`. We now have a
    `Logger` base trait, and an `AbstractLogger` that defines all the
    basic functionalities, making it much easier to add a new subclass of
    `Logger`.
    
    The old `Logger` is renamed to `Log4JLogger`.

--- backend/src/main/scala/bloop/logging/BufferedLogger.scala
+++ backend/src/main/scala/bloop/logging/BufferedLogger.scala
@@ -1,30 +1,25 @@
 package bloop.logging
-import java.util.function.Supplier
+
 import java.util.concurrent.ConcurrentLinkedDeque
 
-class BufferedLogger(underlying: Logger) extends Logger(underlying) {
+class BufferedLogger(underlying: Logger) extends AbstractLogger {
 
   private[this] val buffer = new ConcurrentLinkedDeque[() => Unit]()
 
-  override def debug(msg: String) = buffer.addLast(() => underlying.debug(msg))
-  override def debug(msg: Supplier[String]) = buffer.addLast(() => underlying.debug(msg))
-
-  override def error(msg: String) = buffer.addLast(() => underlying.error(msg))
-  override def error(msg: Supplier[String]) = buffer.addLast(() => underlying.error(msg))
-
-  override def warn(msg: String) = buffer.addLast(() => underlying.warn(msg))
-  override def warn(msg: Supplier[String]) = buffer.addLast(() => underlying.warn(msg))
-
-  override def trace(exception: Throwable) = buffer.addLast(() => underlying.trace(exception))
-  override def trace(exception: Supplier[Throwable]) =
-    buffer.addLast(() => underlying.trace(exception))
-
-  override def info(msg: String) = buffer.addLast(() => underlying.info(msg))
-  override def info(msg: Supplier[String]) = buffer.addLast(() => underlying.info(msg.get))
+  override def name = underlying.name
+  override def ansiCodesSupported() = underlying.ansiCodesSupported()
 
   def clear(): Unit = buffer.clear()
   def flush(): Unit = {
     buffer.forEach(op => op.apply())
     buffer.clear()
   }
+
+  override def debug(msg: String) = buffer.addLast(() => underlying.debug(msg))
+  override def error(msg: String) = buffer.addLast(() => underlying.error(msg))
+  override def warn(msg: String) = buffer.addLast(() => underlying.warn(msg))
+  override def trace(exception: Throwable) = buffer.addLast(() => underlying.trace(exception))
+  override def info(msg: String) = buffer.addLast(() => underlying.info(msg))
+  override def verbose[T](op: => T) = op
+
 }
