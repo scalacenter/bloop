c3ec75a1fa5436f359fadc1a65f6bace0f05d3ec
jvican <jorgevc@fastmail.es>
2017-12-09 12:41:24 +0100

    Add insane infrastructure for log4j

--- backend/src/main/scala/bloop/logging/Log4JLogger.scala
+++ backend/src/main/scala/bloop/logging/Log4JLogger.scala
@@ -1,21 +1,24 @@
 package bloop.logging
 
+import java.io.OutputStream
+
 import org.apache.logging.log4j
 import org.apache.logging.log4j.{Level, LogManager}
-import org.apache.logging.log4j.core.config.Configurator
+import org.apache.logging.log4j.core.LoggerContext
+import org.apache.logging.log4j.core.appender.{OutputStreamAppender, OutputStreamManager}
+import org.apache.logging.log4j.core.layout.PatternLayout
+import org.apache.logging.log4j.core.config.{AppenderRef, Configurator, LoggerConfig}
 
+/** Creates a logger that is backed up by a Log4j logger. */
 class Log4JLogger(override val name: String) extends AbstractLogger {
   def this(logger: Logger) = this(logger.name)
-
   private val logger: log4j.Logger = LogManager.getLogger(name)
 
   override def ansiCodesSupported() = true
-
   override def debug(msg: String): Unit = msg.lines.foreach(logger.debug)
   override def error(msg: String): Unit = msg.lines.foreach(logger.error)
   override def warn(msg: String): Unit = msg.lines.foreach(logger.warn)
   override def trace(exception: Throwable): Unit = logger.trace(exception)
-
   override def info(msg: String): Unit = msg.lines.foreach(logger.info)
 
   override def verbose[T](op: => T): T = {
@@ -23,5 +26,28 @@ class Log4JLogger(override val name: String) extends AbstractLogger {
     Configurator.setRootLevel(Level.DEBUG)
     try op
     finally Configurator.setRootLevel(initialLevel)
+  }
+}
+
+object Lof4JLogger {
+  private val DefaultLayout: String =
+    "%highlight{%equals{[%-0.-1level] }{[I] }{}}{FATAL=white, ERROR=bright red, WARN=yellow, INFO=dim blue, DEBUG=green, TRACE=blue}%msg%n"
+  private final val LoggerName = "org.apache.logging.log4j"
+  private final val AppenderName = "common-options-out"
+  def swapOut(logger: Logger, out: OutputStream): Unit = synchronized(logger) {
+    val ctx = LogManager.getContext(false).asInstanceOf[LoggerContext]
+    val config = ctx.getConfiguration()
+    val layout = PatternLayout.newBuilder().withPattern(DefaultLayout)
+    val manager = new OutputStreamManager(out, AppenderName, layout, true)
+    val appender = new OutputStreamAppender(AppenderName, layout, null, manager, true)
+    appender.start(appender)
+    config.addAppender(appender)
+
+    val refs = Array(AppenderRef.createAppenderRef(AppenderName, null, null))
+    val loggerConfig: LoggerConfig =
+      LoggerConfig.createLogger("false", "debug", Loggername, "true", refs, null, config, null)
+    loggerConfig.addAppender(appender, null, null)
+    config.addLogger(LoggerName, loggerConfig)
+    ctx.updateLoggers()
   }
 }
