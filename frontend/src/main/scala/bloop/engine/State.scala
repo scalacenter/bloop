adccc6e5562abf25dde3885b92aa9d7a2d1d126c
jvican <jorgevc@fastmail.es>
2017-12-09 16:13:12 +0100

    Add `commonOptions` to state and docs
    `CommonOptions` is required in the state so that we can modify the
    logger every time the common options change. This can happen when users
    pass in command redirecting input and output, or when bloop is run by a
    nailgun server.
    
    This is also extremely important for testing. The reconfiguration of
    logging is not done in this PR, but will be done in a follow up.

--- frontend/src/main/scala/bloop/engine/State.scala
+++ frontend/src/main/scala/bloop/engine/State.scala
@@ -1,18 +1,34 @@
 package bloop.engine
 
 import bloop.CompilerCache
-import bloop.cli.ExitStatus
+import bloop.cli.{CommonOptions, ExitStatus}
 import bloop.engine.caches.{ResultsCache, StateCache}
 import bloop.io.Paths
 import bloop.logging.Logger
 
+/**
+ * Represents the state for a given build.
+ *
+ * An state is aggressively cached by bloop so that operations that target a build can reuse
+ * the state that has already been created before. The state is immutable and allows different
+ * builds to change common values utilized by all the bloop operations.
+ *
+ * @param build The build with which the state is associated.
+ * @param results The results cache that contains all the analysis file for a build.
+ * @param compilerCache The cache that has pointers to hot compilers (shared across other states).
+ * @param commonOptions The environment options necessary for input and output.
+ * @param status The status in which the state is currently.
+ * @param logger The logger that is used and is associated with a given build.
+ */
 final case class State private (
     build: Build,
     results: ResultsCache,
     compilerCache: CompilerCache,
+    commonOptions: CommonOptions,
     status: ExitStatus,
     logger: Logger
 ) {
+  /* TODO: Improve the handling and merging of different status. Use the status to report errors. */
   private[bloop] val executionContext: scala.concurrent.ExecutionContext =
     ExecutionContext.threadPool
   def mergeStatus(newStatus: ExitStatus): State =
@@ -37,17 +53,18 @@ object State {
     val initializedResults = build.projects.foldLeft(ResultsCache.getEmpty(logger)) {
       case (results, project) => results.initializeResult(project)
     }
-    State(build, initializedResults, compilerCache, ExitStatus.Ok, logger)
+    State(build, initializedResults, compilerCache, CommonOptions.default, ExitStatus.Ok, logger)
   }
 
   // Improve the caching by using file metadata
   def apply(build: Build, logger: Logger): State = {
-    val initializedResults = build.projects.foldLeft(ResultsCache.getEmpty(logger)) {
+    val results = build.projects.foldLeft(ResultsCache.getEmpty(logger)) {
       case (results, project) => results.initializeResult(project)
     }
 
+    val options = CommonOptions.default
     val compilerCache = getCompilerCache(logger)
-    val stateToCache = State(build, initializedResults, compilerCache, ExitStatus.Ok, logger)
+    val stateToCache = State(build, results, compilerCache, options, ExitStatus.Ok, logger)
     stateCache.updateBuild(state = stateToCache)
     stateToCache
   }
