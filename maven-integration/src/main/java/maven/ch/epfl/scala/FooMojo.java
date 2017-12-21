6ee5a366defc3a1e0733a0e4c08c20db37fb8bf1
jvican <jorgevc@fastmail.es>
2017-12-21 01:33:37 +0100

    Format `FooMojo`

--- maven-integration/src/main/java/maven/ch/epfl/scala/FooMojo.java
+++ maven-integration/src/main/java/maven/ch/epfl/scala/FooMojo.java
@@ -1,18 +1,15 @@
 package maven.ch.epfl.scala;
-     
+
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugins.annotations.Mojo;
-     
+
 /**
-* Says "Hi" to the user.
-*
-*/
-@Mojo( name = "sayhi")
-public class FooMojo extends AbstractMojo
-{
-public void execute() throws MojoExecutionException
-{
-getLog().info( "Hello, world." );
-}
+ * Says "Hi" to the user.
+ */
+@Mojo(name = "sayhi")
+public class FooMojo extends AbstractMojo {
+    public void execute() throws MojoExecutionException {
+        getLog().info("Hello, world.");
+    }
 }
