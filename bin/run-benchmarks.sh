d95724e5c2ec1c82982aed47bc9eefb119924f8e
Martin Duhem <martin.duhem@gmail.com>
2018-01-05 11:49:48 +0100

    Use different JMH options for different benchmarks
    Benchmarks are categorized as `small`, `medium` and `large` based on how
    long they take to run. The scripted `run-benchmarks.sh` now allows to
    specify different JMH options for every of those benchmarks.

--- bin/run-benchmarks.sh
+++ bin/run-benchmarks.sh
@@ -2,35 +2,40 @@
 set -o pipefail
 
 BLOOP_DEFAULT_REFERENCE="master"
-BLOOP_DEFAULT_JMH_OPTIONS="-wi 10 -i 10 -f3 -t1"
+BLOOP_SMALL_JMH_OPTIONS="-wi 10 -i 10 -f3 -t1"
+BLOOP_MEDIUM_JMH_OPTIONS="-wi 10 -i 10 -f2 -t1"
+BLOOP_LARGE_JMH_OPTIONS="-wi 10 -i 10 -f1 -t1"
 
 BLOOP_REFERENCE="$BLOOP_DEFAULT_REFERENCE"
-BLOOP_JMH_OPTIONS="$BLOOP_DEFAULT_JMH_OPTIONS"
 BLOOP_JMH_RUNNER="benchmarks/jmh:run"
 BLOOP_HOME="$HOME/bloop-benchmarks"
 BLOOP_REPO="https://github.com/scalacenter/bloop.git"
 
 usage() {
-    echo "Usage: ./run-benchmarks.sh -r | --ref <git-ref> Build and benchmark the given reference."
-    echo "                                                Defaults to \"$BLOOP_DEFAULT_REFERENCE\""
-    echo "                           --upload             If set, upload the results to InfluxDB."
-    echo "                           -j | --jmh-options   Pass the given options to JMH."
-    echo "                                                Defaults to \"$BLOOP_DEFAULT_JMH_OPTIONS\""
-    echo "                           -h | --help          Show this message and exit."
+    echo "Usage: ./run-benchmarks.sh -r | --ref <git-ref>       Build and benchmark the given reference."
+    echo "                                                      Defaults to \"$BLOOP_DEFAULT_REFERENCE\""
+    echo "                           --upload                   If set, upload the results to InfluxDB."
+    echo "                           -js | --jmh-options-small  Pass the given options to JMH (small projects)."
+    echo "                                                      Defaults to \"$BLOOP_SMALL_JMH_OPTIONS\""
+    echo "                           -jm | --jmh-options-medium Pass the given options to JMH (medium projects)."
+    echo "                                                      Defaults to \"$BLOOP_MEDIUM_JMH_OPTIONS\""
+    echo "                           -jl | --jmh-options-large  Pass the given options to JMH (large projects)."
+    echo "                                                      Defaults to \"$BLOOP_LARGE_JMH_OPTIONS\""
+    echo "                           -h | --help                Show this message and exit."
     echo ""
     echo "Examples:"
     echo "  ./run-benchmarks.sh --ref +refs/pull/42/merge"
     echo "    Build and run the benchmarks on the pull request 42 (after merging)"
     echo "  ./run-benchmarks.sh --ref deadbeef --upload"
     echo "    Build and run the benchmarks on commit \"deadbeef\", uploads the results."
-    echo "  ./run-benchmarks.sh --jmh-options \"-i 1 -wi 1 -f1 -t1\""
-    echo "    Build and run the benchmarks on \"master\", pass \"-i 1 -wi1 -f1 -t1\" to JMH."
+    echo "  ./run-benchmarks.sh --jmh-options-small \"-i 1 -wi 1 -f1 -t1\""
+    echo "    Build and run the benchmarks on \"master\", runs small benchmarks with \"-i 1 -wi1 -f1 -t1\"."
 }
 
 main() {
     mkdir -p "$BLOOP_HOME"
 
-    JMH_CMD="$BLOOP_JMH_RUNNER $BLOOP_JMH_OPTIONS"
+    JMH_CMD="$BLOOP_JMH_RUNNER"
     LOG_FILE="$BLOOP_HOME/$(date +"%F-%H%M%S")-benchmarks-log.txt"
     TEMP_DIR=$(mktemp -d)
     COMMANDS_FILE="$TEMP_DIR/commands"
@@ -45,28 +50,28 @@ main() {
     echo "install" >> "$COMMANDS_FILE"
     echo "setupTests" >> "$COMMANDS_FILE"
 
-    SCALAC_SBT_BLOOP_BENCHMARKS=("-p project=scala -p projectName=library"
-                                 "-p project=mini-better-files -p projectName=mini-better-files")
+    SCALAC_SBT_BLOOP_BENCHMARKS=("$BLOOP_LARGE_JMH_OPTIONS -p project=scala -p projectName=library"
+                                 "$BLOOP_SMALL_JMH_OPTIONS -p project=mini-better-files -p projectName=mini-better-files")
     for benchmark in "${SCALAC_SBT_BLOOP_BENCHMARKS[@]}"
     do
         echo "$JMH_CMD .*Hot.*Benchmark.* $benchmark" >> "$COMMANDS_FILE"
     done
 
-    SBT_BLOOP_BENCHMARKS=("-p project=sbt -p projectName=sbtRoot"
-                          "-p project=scala -p projectName=compiler"
-                          "-p project=utest -p projectName=root"
-                          "-p project=versions -p projectName=versions"
-                          "-p project=with-tests -p projectName=with-tests"
-                          "-p project=frontend -p projectName=main"
-                          "-p project=spark -p projectName=examples")
+    SBT_BLOOP_BENCHMARKS=("$BLOOP_MEDIUM_JMH_OPTIONS -p project=sbt -p projectName=sbtRoot"
+                          "$BLOOP_LARGE_JMH_OPTIONS -p project=scala -p projectName=compiler"
+                          "$BLOOP_SMALL_JMH_OPTIONS -p project=utest -p projectName=root"
+                          "$BLOOP_SMALL_JMH_OPTIONS -p project=versions -p projectName=versions"
+                          "$BLOOP_SMALL_JMH_OPTIONS -p project=with-tests -p projectName=with-tests"
+                          "$BLOOP_LARGE_JMH_OPTIONS -p project=frontend -p projectName=main"
+                          "$BLOOP_LARGE_JMH_OPTIONS -p project=spark -p projectName=examples")
     for benchmark in "${SBT_BLOOP_BENCHMARKS[@]}"
     do
         echo "$JMH_CMD .*Hot(Sbt|Bloop)Benchmark.* $benchmark" >> "$COMMANDS_FILE"
     done
 
-    BLOOP_BENCHMARKS=("bloop.ProjectBenchmark"
-                      "bloop.engine.tasks.TaskBenchmark"
-                      "bloop.logging.BloopLoggerBenchmark")
+    BLOOP_BENCHMARKS=("$BLOOP_SMALL_JMH_OPTIONS bloop.ProjectBenchmark"
+                      "$BLOOP_SMALL_JMH_OPTIONS bloop.engine.tasks.TaskBenchmark"
+                      "$BLOOP_SMALL_JMH_OPTIONS bloop.logging.BloopLoggerBenchmark")
     for benchmark in "${BLOOP_BENCHMARKS[@]}"
     do
         echo "$JMH_CMD $benchmark" >> "$COMMANDS_FILE"
@@ -82,19 +87,25 @@ main() {
 
 while [ "$1" != "" ]; do
     case $1 in
-        -r | --ref )         shift
-                             BLOOP_REFERENCE=$1
-                             ;;
-        --upload )           BLOOP_JMH_RUNNER="benchmarks/jmh:runMain scala.bench.UploadingRunner"
-                             ;;
-        -j | --jmh-options ) shift
-                             BLOOP_JMH_OPTIONS=$1
-                             ;;
-        -h | --help )        usage
-                             exit 0
-                             ;;
-        * )                  usage
-                             exit 1
+        -r | --ref )                 shift
+                                     BLOOP_REFERENCE=$1
+                                     ;;
+        --upload )                   BLOOP_JMH_RUNNER="benchmarks/jmh:runMain scala.bench.UploadingRunner"
+                                     ;;
+        -js | --jmh-options-small )  shift
+                                     BLOOP_SMALL_JMH_OPTIONS=$1
+                                     ;;
+        -jm | --jmh-options-medium ) shift
+                                     BLOOP_MEDIUM_JMH_OPTIONS=$1
+                                     ;;
+        -jl | --jmh-options-large )  shift
+                                     BLOOP_LARGE_JMH_OPTIONS=$1
+                                     ;;
+        -h | --help )                usage
+                                     exit 0
+                                     ;;
+        * )                          usage
+                                     exit 1
     esac
     shift
 done
