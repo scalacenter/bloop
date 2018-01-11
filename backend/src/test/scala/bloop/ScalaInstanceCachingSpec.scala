c3c072ac1a1606f9ebdeea2043131d9f4771cb82
Martin Duhem <martin.duhem@gmail.com>
2018-01-11 10:51:35 +0100

    Port `ScalaInstanceCachingSpec` to JUnit

--- backend/src/test/scala/bloop/ScalaInstanceCachingSpec.scala
+++ backend/src/test/scala/bloop/ScalaInstanceCachingSpec.scala
@@ -1,30 +1,59 @@
 package bloop
 
+import java.util.{Arrays, Collection}
+
 import bloop.logging.BloopLogger
-import utest.{TestSuite, Tests, TestableString, assert}
+import org.junit.Assert.{assertSame, assertTrue}
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.junit.runners.Parameterized
+import org.junit.runners.Parameterized.Parameters
 
-object ScalaInstanceCachingSpec extends TestSuite {
-  val sameVersionPairs = List("2.10.6", "2.10.1", "2.11.11", "2.12.0", "2.12.4")
-  val unsharedVersionPairs = List("2.10.6" -> "2.10.4", "2.11.11" -> "2.11.6", "2.12.4" -> "2.12.1")
-  private val logger = BloopLogger.default("test-logger")
-  val tests = Tests {
-    "Scala instances are cached" - {
-      "Instances of the same Scala version must share classloaders" - {
-        for (version <- sameVersionPairs) {
-          val instance1 = ScalaInstance.resolve("org.scala-lang", "scala-compiler", version, logger)
-          val instance2 = ScalaInstance.resolve("org.scala-lang", "scala-compiler", version, logger)
-          assert(instance1.loader == instance2.loader)
-        }
+object ScalaInstanceCachingSpec {
+  val sameVersionPairs = Array("2.10.6", "2.10.1", "2.11.11", "2.12.0", "2.12.4")
+  val unsharedVersionPairs =
+    Array("2.10.6" -> "2.10.4", "2.11.11" -> "2.11.6", "2.12.4" -> "2.12.1")
+  val logger = BloopLogger.default("test-logger")
+}
 
-      }
+@RunWith(classOf[Parameterized])
+class SameScalaVersions(version: String) {
+  @Test
+  def sameScalaVersionsShareClassLoaders = {
+    val instance1 = ScalaInstance.resolve("org.scala-lang",
+                                          "scala-compiler",
+                                          version.toString,
+                                          ScalaInstanceCachingSpec.logger)
+    val instance2 = ScalaInstance.resolve("org.scala-lang",
+                                          "scala-compiler",
+                                          version.toString,
+                                          ScalaInstanceCachingSpec.logger)
+    assertSame(s"Failed with version = $version", instance1.loader, instance2.loader)
+  }
+}
+object SameScalaVersions {
+  @Parameters
+  def data(): Collection[Array[String]] = {
+    Arrays.asList(ScalaInstanceCachingSpec.sameVersionPairs.map(Array.apply(_)): _*)
+  }
+}
 
-      "Different Scala instances with the same major number do not share the same classloader" - {
-        for ((v1, v2) <- unsharedVersionPairs) {
-          val instance1 = ScalaInstance.resolve("org.scala-lang", "scala-compiler", v1, logger)
-          val instance2 = ScalaInstance.resolve("org.scala-lang", "scala-compiler", v2, logger)
-          assert(instance1.loader != instance2.loader)
-        }
-      }
-    }
+@RunWith(classOf[Parameterized])
+class DifferentScalaVersions(v1: String, v2: String) {
+  @Test
+  def differentVersionSameMajorNumberDontShareClassLoaders = {
+    val instance1 =
+      ScalaInstance.resolve("org.scala-lang", "scala-compiler", v1, ScalaInstanceCachingSpec.logger)
+    val instance2 =
+      ScalaInstance.resolve("org.scala-lang", "scala-compiler", v2, ScalaInstanceCachingSpec.logger)
+    assertTrue(s"Failed with v1 = $v1, v2 = $v2", instance1.loader != instance2.loader)
+  }
+}
+object DifferentScalaVersions {
+  @Parameters
+  def data(): Collection[Array[String]] = {
+    Arrays.asList(ScalaInstanceCachingSpec.unsharedVersionPairs.map {
+      case (v1, v2) => Array(v1, v2)
+    }: _*)
   }
 }
