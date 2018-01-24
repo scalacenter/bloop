8dea895f14017981c25e4e98e7c048eaba649c2f
Martin Duhem <martin.duhem@gmail.com>
2018-01-24 15:52:29 +0100

    Format installation script

--- bin/install.py
+++ bin/install.py
@@ -8,46 +8,57 @@ from subprocess import CalledProcessError, check_call
 import sys
 
 BLOOP_DEFAULT_INSTALLATION_TARGET = join(expanduser("~"), ".bloop")
-BLOOP_LATEST_RELEASE              = "no-tag-yet"
-COURSIER_URL                      = "https://git.io/vgvpD"
-NAILGUN_COMMIT                    = "0927946db663927151a53fe3b365b2655613db86"
-NAILGUN_CLIENT_URL                = "https://raw.githubusercontent.com/scalacenter/nailgun/%s/pynailgun/ng.py" % NAILGUN_COMMIT
+BLOOP_LATEST_RELEASE = "no-tag-yet"
+COURSIER_URL = "https://git.io/vgvpD"
+NAILGUN_COMMIT = "0927946db663927151a53fe3b365b2655613db86"
+NAILGUN_CLIENT_URL = "https://raw.githubusercontent.com/scalacenter/nailgun/%s/pynailgun/ng.py" % NAILGUN_COMMIT
 
 parser = argparse.ArgumentParser(description="Installation script for Bloop.")
-parser.add_argument('-d', '--dest',
-        default = BLOOP_DEFAULT_INSTALLATION_TARGET,
-        help = "Where to install Bloop, defaults to %s" % BLOOP_DEFAULT_INSTALLATION_TARGET)
-parser.add_argument('-v', '--version',
-        default = BLOOP_LATEST_RELEASE,
-        help = "Version of Bloop to install, defaults to %s" % BLOOP_LATEST_RELEASE)
+parser.add_argument(
+    '-d',
+    '--dest',
+    default=BLOOP_DEFAULT_INSTALLATION_TARGET,
+    help="Where to install Bloop, defaults to %s" %
+    BLOOP_DEFAULT_INSTALLATION_TARGET)
+parser.add_argument(
+    '-v',
+    '--version',
+    default=BLOOP_LATEST_RELEASE,
+    help="Version of Bloop to install, defaults to %s" % BLOOP_LATEST_RELEASE)
 args = parser.parse_args()
 
 BLOOP_INSTALLATION_TARGET = args.dest
-BLOOP_VERSION             = args.version
+BLOOP_VERSION = args.version
 
-BLOOP_COURSIER_TARGET     = join(BLOOP_INSTALLATION_TARGET, "coursier")
-BLOOP_SERVER_TARGET       = join(BLOOP_INSTALLATION_TARGET, "bloop-server")
-BLOOP_SHELL_TARGET        = join(BLOOP_INSTALLATION_TARGET, "bloop-shell")
-BLOOP_CLIENT_TARGET       = join(BLOOP_INSTALLATION_TARGET, "bloop-ng.py")
+BLOOP_COURSIER_TARGET = join(BLOOP_INSTALLATION_TARGET, "coursier")
+BLOOP_SERVER_TARGET = join(BLOOP_INSTALLATION_TARGET, "bloop-server")
+BLOOP_SHELL_TARGET = join(BLOOP_INSTALLATION_TARGET, "bloop-shell")
+BLOOP_CLIENT_TARGET = join(BLOOP_INSTALLATION_TARGET, "bloop-ng.py")
 
 BLOOP_ARTIFACT = "ch.epfl.scala:bloop-frontend_2.12:%s" % BLOOP_VERSION
+
 
 def download_and_install(url, target):
     try:
         urllib.urlretrieve(url, target)
         os.chmod(target, 0755)
     except IOError:
-        print "Couldn't download %s, please try again." %url
+        print "Couldn't download %s, please try again." % url
         sys.exit(1)
+
 
 def coursier_bootstrap(target, main):
     try:
-        check_call([BLOOP_COURSIER_TARGET, "bootstrap", BLOOP_ARTIFACT, "-o", target, "--standalone", "--main", main])
+        check_call([
+            "java", "-jar", BLOOP_COURSIER_TARGET, "bootstrap", BLOOP_ARTIFACT,
+            "-o", target, "--standalone", "--main", main
+        ])
     except CalledProcessError as e:
         print "Coursier couldn't create %s. Please report an issue." % target
         print "Command: %s" % e.cmd
         print "Return code: %d" % e.returncode
         sys.exit(e.returncode)
+
 
 if not isdir(BLOOP_INSTALLATION_TARGET):
     os.makedirs(BLOOP_INSTALLATION_TARGET)
@@ -66,4 +77,3 @@ if not isfile(BLOOP_SHELL_TARGET):
 if not isfile(BLOOP_CLIENT_TARGET):
     download_and_install(NAILGUN_CLIENT_URL, BLOOP_CLIENT_TARGET)
     print "Installed bloop client in '%s'" % BLOOP_CLIENT_TARGET
-
