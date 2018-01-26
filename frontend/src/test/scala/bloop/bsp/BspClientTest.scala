0d5cc667da9854972587d2bcdc9c23980bb9230d
jvican <jorgevc@fastmail.es>
2018-01-26 14:37:48 +0100

    Add some sleeps to make sure bsp works
    We add the sleeps because we don't want to use a bigger scheduler. The
    bsp scheduler is limited to 4 threads on purpose to make sure that the
    our implementation is not leaking and leaving any task running on the
    background.

--- frontend/src/test/scala/bloop/bsp/BspClientTest.scala
+++ frontend/src/test/scala/bloop/bsp/BspClientTest.scala
@@ -42,6 +42,8 @@ object BspClientTest {
 
   def runTest[T](cmd: Commands.ValidatedBsp, configDirectory: AbsolutePath)(
       runEndpoints: LanguageClient => me.Task[Either[Response.Error, T]]): Unit = {
+    // Let's sleep to give time to bspScheduler to cancel tasks while running several tests sequentially
+    Thread.sleep(200)
 
     val scheduler = ExecutionContext.bspScheduler
     val projectName = cmd.cliOptions.common.workingPath.underlying.getFileName().toString()
@@ -54,7 +56,7 @@ object BspClientTest {
       val messages = BaseProtocolMessage.fromInputStream(socket.getInputStream)
       val lsServer = new LanguageServer(messages, lsClient, services, scheduler, slf4jLogger)
       val runningClientServer = lsServer.startTask.runAsync(scheduler)
-      Thread.sleep(200)
+      Thread.sleep(400)
 
       val cwd = configDirectory.getParent
       val initializeServer = endpoints.Build.initialize.request(
@@ -97,6 +99,6 @@ object BspClientTest {
     }
 
     // We delay the start of the client to wait for the bsp server to go live.
-    connectToServer.delayExecution(scala.concurrent.duration.FiniteDuration(1000, "ms"))
+    connectToServer.delayExecution(scala.concurrent.duration.FiniteDuration(500, "ms"))
   }
 }
