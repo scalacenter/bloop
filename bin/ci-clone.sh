1de23e563f02dd0134371081e2726b78c58844a1
Martin Duhem <martin.duhem@gmail.com>
2018-01-30 17:36:26 +0100

    Do the recursive cloning ourselves
    And do shallow clones of the submodules

---
+++ bin/ci-clone.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env sh
+# Taken from lampepfl/dotty
+set -eux
+
+# if build is PR rebase on top of target branch
+if [ "$DRONE_BUILD_EVENT" = "pull_request" ]; then
+  git config user.email "bloopoid@trashmail.ws"
+  git config user.name "Bloopoid"
+  git pull "$DRONE_REMOTE_URL" "$DRONE_BRANCH"
+fi
+
+# clone submodules in parallel
+git submodule update --init --recursive --jobs 4 --depth 50
