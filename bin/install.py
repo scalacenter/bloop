984b0ab494caf04ba858aa975c712bca5205b73d
Martin Duhem <martin.duhem@gmail.com>
2018-03-14 22:42:01 +0100

    Install the completions in the installation script

--- bin/install.py
+++ bin/install.py
@@ -7,6 +7,7 @@ import os
 from os.path import expanduser, isdir, isfile, join
 from subprocess import CalledProcessError, check_call
 import sys
+import re
 
 IS_PY2 = sys.version[0] == '2'
 if IS_PY2:
@@ -53,11 +54,30 @@ args = parser.parse_args()
 BLOOP_INSTALLATION_TARGET = args.dest
 BLOOP_VERSION = args.version
 NAILGUN_COMMIT = args.nailgun
+ZSH_COMPLETION_DIR = join(BLOOP_INSTALLATION_TARGET, "zsh")
+BASH_COMPLETION_DIR = join(BLOOP_INSTALLATION_TARGET, "bash")
+
+# If this is not a released version of Bloop, we need to extract the commit SHA
+# to know how to download the completion scripts.
+# If we can't get the SHA, just download from master.
+if CUSTOMIZED_SCRIPT:
+    COMPLETION_VERSION = BLOOP_VERSION
+else:
+    pattern = '(?:.+?)-([0-9a-f]{8})(?:\+\d{8}-\d{4})?'
+    matches = re.search(pattern, BLOOP_VERSION)
+    if matches is not None:
+        COMPLETION_VERSION = matches.group(1)
+    else:
+        COMPLETION_VERSION = "master"
 
 NAILGUN_CLIENT_URL = "https://raw.githubusercontent.com/scalacenter/nailgun/%s/pynailgun/ng.py" % NAILGUN_COMMIT
+ZSH_COMPLETION_URL = "https://raw.githubusercontent.com/scalacenter/bloop/%s/etc/zsh/_bloop" % COMPLETION_VERSION
+BASH_COMPLETION_URL = "https://raw.githubusercontent.com/scalacenter/bloop/%s/etc/bash/bloop" % COMPLETION_VERSION
 BLOOP_COURSIER_TARGET = join(BLOOP_INSTALLATION_TARGET, "blp-coursier")
 BLOOP_SERVER_TARGET = join(BLOOP_INSTALLATION_TARGET, "blp-server")
 BLOOP_CLIENT_TARGET = join(BLOOP_INSTALLATION_TARGET, "bloop")
+ZSH_COMPLETION_TARGET = join(ZSH_COMPLETION_DIR, "_bloop")
+BASH_COMPLETION_TARGET = join(BASH_COMPLETION_DIR, "bloop")
 
 BLOOP_ARTIFACT = "ch.epfl.scala:bloop-frontend_2.12:%s" % BLOOP_VERSION
 
@@ -83,8 +103,13 @@ def coursier_bootstrap(target, main):
         print("Return code: %d" % e.returncode)
         sys.exit(e.returncode)
 
-if not isdir(BLOOP_INSTALLATION_TARGET):
-    os.makedirs(BLOOP_INSTALLATION_TARGET)
+def makedir(directory):
+    if not isdir(directory):
+        os.makedirs(directory)
+
+makedir(BLOOP_INSTALLATION_TARGET)
+makedir(ZSH_COMPLETION_DIR)
+makedir(BASH_COMPLETION_DIR)
 
 if not isfile(BLOOP_COURSIER_TARGET):
     download_and_install(COURSIER_URL, BLOOP_COURSIER_TARGET)
@@ -94,3 +119,9 @@ print("Installed bloop server in '%s'" % BLOOP_SERVER_TARGET)
 
 download_and_install(NAILGUN_CLIENT_URL, BLOOP_CLIENT_TARGET)
 print("Installed bloop client in '%s'" % BLOOP_CLIENT_TARGET)
+
+download_and_install(ZSH_COMPLETION_URL, ZSH_COMPLETION_TARGET)
+print("Installed zsh completion in '%s'" % ZSH_COMPLETION_TARGET)
+
+download_and_install(BASH_COMPLETION_URL, BASH_COMPLETION_TARGET)
+print("Installed Bash completion in '%s'" % BASH_COMPLETION_TARGET)
