857d268cf28b6917b276a59543d2291f2b7cef7a
jvican <jorgevc@fastmail.es>
2018-03-15 13:49:16 +0100

    Reimplement `copyResources` ourselves
    We need to do this. Yeah, I know, it's ugly. But it's the only way to
    reliably get the resources from sbt.

--- integrations/sbt-bloop/src/main/scala/bloop/integrations/sbt/SbtBloop.scala
+++ integrations/sbt-bloop/src/main/scala/bloop/integrations/sbt/SbtBloop.scala
@@ -18,7 +18,15 @@ object AutoImported {
   val bloopConfigDir: SettingKey[File] =
     settingKey[File]("Directory where to write bloop configuration files")
   val bloopProductDirectories: TaskKey[Seq[File]] =
-    taskKey[Seq[File]]("Directory where to write the class files")
+    taskKey[Seq[File]]("Bloop product directories")
+  val bloopManagedResourceDirectories: SettingKey[Seq[File]] =
+    settingKey[Seq[File]]("Managed resource directories for bloop")
+  val bloopClassDirectory: SettingKey[File] =
+    settingKey[File]("Directory where to write the class files")
+  val bloopTargetDir: SettingKey[File] =
+    settingKey[File]("Target directory for the pertinent project and configuration")
+  val bloopResourceManaged: SettingKey[File] =
+    settingKey[File]("Resource managed for bloop")
   val bloopInternalClasspath: TaskKey[Seq[(File, File)]] =
     taskKey[Seq[(File, File)]]("Directory where to write the class files")
   val bloopInstall: TaskKey[Unit] =
@@ -33,18 +41,22 @@ object PluginImplementation {
     BloopKeys.bloopInstall := PluginDefaults.bloopInstall.value
   )
 
+  import Compat._
   val configSettings: Seq[Def.Setting[_]] = {
     val rawSettingsInConfigs = List(
-      BloopKeys.bloopProductDirectories := PluginDefaults.generateBloopProductDirectories.value,
+      BloopKeys.bloopProductDirectories := List(BloopKeys.bloopClassDirectory.value),
+      BloopKeys.bloopManagedResourceDirectories := PluginDefaults.managedResourceDirs.value,
+      BloopKeys.bloopClassDirectory := PluginDefaults.generateBloopProductDirectories.value,
       BloopKeys.bloopInternalClasspath := PluginDefaults.bloopInternalDependencyClasspath.value,
+      BloopKeys.bloopResourceManaged := BloopKeys.bloopTargetDir.value / "resource_managed",
       BloopKeys.bloopGenerate := PluginDefaults.bloopGenerate.value
     )
     val all = rawSettingsInConfigs ++ DiscoveredSbtPlugins.settings
     all.flatMap(ss => sbt.inConfig(Compile)(ss) ++ sbt.inConfig(Test)(ss))
   }
 
-  import Compat._
   val projectSettings: Seq[Def.Setting[_]] = configSettings ++ List(
+    BloopKeys.bloopTargetDir := PluginDefaults.bloopTargetDir.value,
     BloopKeys.bloopConfigDir := Def.settingDyn {
       val ref = Keys.thisProjectRef.value
       Def.setting {
@@ -60,20 +72,24 @@ object PluginImplementation {
     import Compat._
     import sbt.{Task, Defaults}
 
-    lazy val generateBloopProductDirectories: Def.Initialize[Task[Seq[File]]] = Def.task {
+    lazy val bloopTargetDir: Def.Initialize[File] = Def.setting {
       val project = Keys.thisProject.value
-      val configuration = Keys.configuration.value
       val bloopConfigDir = BloopKeys.bloopConfigDir.value
-      val bloopTarget = Defaults.makeCrossTarget(
+      Defaults.makeCrossTarget(
         bloopConfigDir / project.id,
         Keys.scalaBinaryVersion.value,
         (Keys.sbtBinaryVersion in Keys.pluginCrossBuild).value,
         Keys.sbtPlugin.value,
         Keys.crossPaths.value
       )
+    }
+
+    lazy val generateBloopProductDirectories: Def.Initialize[File] = Def.setting {
+      val configuration = Keys.configuration.value
+      val bloopTarget = AutoImported.bloopTargetDir.value
       val classesDir = bloopTarget / (Defaults.prefix(configuration.name) + "classes")
       if (!classesDir.exists()) sbt.IO.createDirectory(classesDir)
-      List(classesDir)
+      classesDir
     }
 
     lazy val bloopGenerate: Def.Initialize[Task[Unit]] = Def.task {
@@ -132,7 +148,7 @@ object PluginImplementation {
       Keys.managedSources.value
 
       // Copy the resources, so that they're available when running and testing
-      val _ = Keys.copyResources.value
+      bloopCopyResourcesTask.value
 
       // format: OFF
       val config = BloopConfig(projectName, baseDirectory, dependenciesAndAggregates, scalaOrg,
@@ -193,6 +209,40 @@ object PluginImplementation {
       val internalClasspath = AutoImported.bloopInternalClasspath.value.map(_._2)
       val externalClasspath = Keys.externalDependencyClasspath.value.map(_.data)
       internalClasspath ++ externalClasspath
+    }
+
+    def bloopCopyResourcesTask = Def.taskDyn {
+      val configKey = sbt.ConfigKey(Keys.configuration.value.name)
+      Def.task {
+        import sbt._
+        val t = AutoImported.bloopClassDirectory.value
+        val dirs =
+          Classpaths
+            .concatSettings(Keys.unmanagedResourceDirectories.in(configKey),
+                            AutoImported.bloopManagedResourceDirectories.in(configKey))
+            .value
+        val s = Keys.streams.value
+        val cacheStore = Compat.generateCacheFile(s, "copy-resources-bloop")
+        val mappings = (sbt.PathFinder(Keys.resources.value) --- dirs) pair (sbt.Path
+          .rebase(dirs, t) | sbt.Path.flat(t))
+        s.log.debug("Copy resource mappings: " + mappings.mkString("\n\t", "\n\t", ""))
+        sbt.Sync(cacheStore)(mappings)
+        mappings
+      }
+    }
+
+    def managedResourceDirs: Def.Initialize[Seq[File]] = Def.settingDyn {
+      val configName = Keys.configuration.value.name
+      val configKey = sbt.ConfigKey(configName)
+      Def.setting {
+        val oldUnmanagedResourceDirs = Keys.managedResourceDirectories.in(configKey).value
+        val oldResourceDir = Keys.resourceManaged.in(configKey).value
+        val newResourceDir =
+          AutoImported.bloopResourceManaged.in(configKey).value / Defaults.nameForSrc(configName)
+        oldUnmanagedResourceDirs.map { dir =>
+          if (dir == oldResourceDir) newResourceDir else dir
+        }
+      }
     }
 
     private type JavaConfiguration = (File, Seq[String])
