63b8486d55f4197775192fe716816cd4f86903c9
jvican <jorgevc@fastmail.es>
2018-04-06 19:09:28 +0200

    Fix bloopGenerate definition in scala/scala
    Error:
    
    ```scala
    /drone/.sbt/1.0/staging/6a8c90d140e2ff5a7832/scala/build.sbt:322: error: type mismatch;
     found   : Unit
     required: sbt.File
        (which expands to)  java.io.File
      bloopGenerate in Compile := {()},
                                   ^
    /drone/.sbt/1.0/staging/6a8c90d140e2ff5a7832/scala/build.sbt:323: error: type mismatch;
     found   : Unit
     required: sbt.File
        (which expands to)  java.io.File
      bloopGenerate in Test := {()}
                                ^
    sbt.compiler.EvalException: Type error in expression
    ```

--- build-integrations/sbt-0.13/project/Integrations.scala
+++ build-integrations/sbt-0.13/project/Integrations.scala
@@ -7,7 +7,7 @@ object Integrations {
   val LihaoyiUtest = RootProject(
     uri("git://github.com/lihaoyi/utest.git#b5440d588d5b32c85f6e9392c63edd3d3fed3106"))
   val ScalaScala = RootProject(
-    uri("git://github.com/scalacenter/scala.git#36bd1ad97c38b177a24a7762c74a9668de330f31"))
+    uri("git://github.com/scalacenter/scala.git#430deb1f3ff23ae0876434b302808d7b004337f6"))
   val ScalaCenterVersions = RootProject(
     uri("git://github.com/scalacenter/versions.git#c296028a33b06ba3a41d399d77c21f6b7100c001"))
 
