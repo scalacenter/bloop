dd6c4e4940c455d8e2f17b32bfa61a90f7cf8f4d
Paweł Bartkiewicz <tuuresairon@gmail.com>
2018-04-19 00:16:10 +0200

    Rework download_and_install_template in install.py
    Downloads templates to temporary files before processing them.
    
    Extracts separate function (download) for downloading files from
    download_and_install so it can be used for
    download_and_install_template.
    
    Makes it possible to specify permissions of downloaded files, so we do
    not set executable bit for images etc.

--- bin/install.py
+++ bin/install.py
@@ -12,10 +12,8 @@ import re
 IS_PY2 = sys.version[0] == '2'
 if IS_PY2:
     from urllib import urlretrieve as urlretrieve
-    from urllib import urlopen as urlopen
 else:
     from urllib.request import urlretrieve as urlretrieve
-    from urllib.request import urlopen as urlopen
 
 # INSERT_INSTALL_VARIABLES
 BLOOP_DEFAULT_INSTALLATION_TARGET = join(expanduser("~"), ".bloop")
@@ -86,29 +84,29 @@ SYSTEMD_SERVICE_TARGET = join(SYSTEMD_SERVICE_DIR, "bloop.service")
 
 BLOOP_ARTIFACT = "ch.epfl.scala:bloop-frontend_2.12:%s" % BLOOP_VERSION
 
+def download(url, target):
+    try:
+        urlretrieve(url, target)
+    except IOError:
+        print("Couldn't download %s, please try again." % url)
+        sys.exit(1)
+
+def download_and_install(url, target, permissions=0o644):
+    download(url, target)
+    os.chmod(target, permissions)
+
 def replace_template_variables(template):
     return template.replace("__BLOOP_INSTALLATION_TARGET__", BLOOP_INSTALLATION_TARGET)
 
-def download_and_install(url, target):
-    try:
-        urlretrieve(url, target)
-        os.chmod(target, 0o755)
-    except IOError:
-        print("Couldn't download %s, please try again." % url)
-        sys.exit(1)
-
-def download_and_install_template(url, target):
-    try:
-        socket = urlopen(url)
-        template = socket.read()
-        with open(target, "w") as file:
-            output = replace_template_variables(template)
-            file.write(output)
-        socket.close()
-        os.chmod(target, 0o755)
-    except IOError:
-        print("Couldn't download %s, please try again." % url)
-        sys.exit(1)
+def download_and_install_template(url, target, permissions=0o644):
+    template_target = target + ".tmp"
+    download(url, template_target)
+    with open(template_target, "r") as template_file, open(target, "w") as output_file:
+        template = template_file.read()
+        output = replace_template_variables(template)
+        output_file.write(output)
+    os.remove(template_target)
+    os.chmod(target, permissions)
 
 def coursier_bootstrap(target, main):
     try:
@@ -135,18 +133,18 @@ makedir(BASH_COMPLETION_DIR)
 makedir(SYSTEMD_SERVICE_DIR)
 
 if not isfile(BLOOP_COURSIER_TARGET):
-    download_and_install(COURSIER_URL, BLOOP_COURSIER_TARGET)
+    download_and_install(COURSIER_URL, BLOOP_COURSIER_TARGET, 0o755)
 
 coursier_bootstrap(BLOOP_SERVER_TARGET, "bloop.Server")
 print("Installed bloop server in '%s'" % BLOOP_SERVER_TARGET)
 
-download_and_install(NAILGUN_CLIENT_URL, BLOOP_CLIENT_TARGET)
+download_and_install(NAILGUN_CLIENT_URL, BLOOP_CLIENT_TARGET, 0o755)
 print("Installed bloop client in '%s'" % BLOOP_CLIENT_TARGET)
 
-download_and_install(ZSH_COMPLETION_URL, ZSH_COMPLETION_TARGET)
+download_and_install(ZSH_COMPLETION_URL, ZSH_COMPLETION_TARGET, 0o755)
 print("Installed zsh completion in '%s'" % ZSH_COMPLETION_TARGET)
 
-download_and_install(BASH_COMPLETION_URL, BASH_COMPLETION_TARGET)
+download_and_install(BASH_COMPLETION_URL, BASH_COMPLETION_TARGET, 0o755)
 print("Installed Bash completion in '%s'" % BASH_COMPLETION_TARGET)
 
 download_and_install_template(SYSTEMD_SERVICE_URL, SYSTEMD_SERVICE_TARGET)
