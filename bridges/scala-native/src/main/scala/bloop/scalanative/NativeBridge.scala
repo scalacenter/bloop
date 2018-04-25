0a16ab4e9e853a052fef47eb381423ac8656dafa
Martin Duhem <martin.duhem@gmail.com>
2018-04-25 12:21:42 +0200

    Add implementation of `NativeBridge`
    The implementation is copy-pasted from the example of how to use the
    build API in Scala Native.

--- bridges/scala-native/src/main/scala/bloop/scalanative/NativeBridge.scala
+++ bridges/scala-native/src/main/scala/bloop/scalanative/NativeBridge.scala
@@ -4,10 +4,39 @@ import bloop.Project
 import bloop.io.AbsolutePath
 import bloop.logging.Logger
 
+import java.nio.file.Path
+
+import scala.scalanative.build.{Discover, Build, Config, GC, Mode}
+
 object NativeBridge {
 
-  def nativeLink(project: Project, entry: String, logger: Logger): AbsolutePath = {
-    ???
+  def nativeLink(project: Project, entry: String, logger: Logger): Path = {
+    val classpath = project.classpath.map(_.underlying)
+    val workdir = project.out.resolve("native").underlying
+
+    val clang = Discover.clang()
+    val clangpp = Discover.clangpp()
+    val linkopts = Discover.linkingOptions()
+    val compopts = Discover.compileOptions()
+    val triple = Discover.targetTriple(clang, workdir)
+    val nativelib = Discover.nativelib(classpath).get
+    val outpath = workdir.resolve("out")
+
+    val config =
+      Config.empty
+        .withGC(GC.default)
+        .withMode(Mode.default)
+        .withClang(clang)
+        .withClangPP(clangpp)
+        .withLinkingOptions(linkopts)
+        .withCompileOptions(compopts)
+        .withTargetTriple(triple)
+        .withNativelib(nativelib)
+        .withMainClass(entry)
+        .withClassPath(classpath)
+        .withWorkdir(workdir)
+
+    Build.build(config, outpath)
   }
 
 }
