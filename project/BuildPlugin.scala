2de1519ba25cb28957a6a4a8fb441a3d343b61a5
jvican <jorgevc@fastmail.es>
2018-05-08 00:38:22 +0200

    Decrease heap memory for all integration builds

--- project/BuildPlugin.scala
+++ project/BuildPlugin.scala
@@ -299,7 +299,7 @@ object BuildImplementation {
       "-Ywarn-numeric-widen" :: "-Ywarn-value-discard" :: "-Xfuture" :: Nil
   )
 
-  final val jvmOptions = "-Xmx6g" :: "-Xms2g" :: "-XX:ReservedCodeCacheSize=512m" :: "-XX:MaxInlineLevel=20" :: Nil
+  final val jvmOptions = "-Xmx4g" :: "-Xms2g" :: "-XX:ReservedCodeCacheSize=512m" :: "-XX:MaxInlineLevel=20" :: Nil
 
   object BuildDefaults {
     import sbt.librarymanagement.Artifact
