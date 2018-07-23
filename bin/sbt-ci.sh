9e159c707e6e78aac449cdfa39ca84b60c939cc6
Jorge Vicente Cantero <jorgevc@fastmail.es>
2018-07-24 00:00:02 +0200

    Move sbt logic to independent script
    And bring back the trick to skip running tests if the string DOCS is
    detected in the commit message.

---
+++ bin/sbt-ci.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+set -o nounset
+
+# I don't trust $DRONE_COMMIT_MESSAGE here, it's had problems in the future
+if git log -1 --pretty=%B | grep DOCS --silent; then
+  echo "Ignoring sbt invocation, docs change was detected so we only update the docs site."
+  sbt -sbt-dir /drone/.sbt/1.0 -sbt-boot /drone/.sbt/boot docs/makeSite
+else
+  sbt -sbt-dir /drone/.sbt/1.0 -sbt-boot /drone/.sbt/boot $@
+  find "/root/.ivy2/cache"     -name "ivydata-*.properties" -print -delete
+  find "/root/.coursier"       -name "ivydata-*.properties" -print -delete
+  find "/root/.sbt"            -name "*.lock"               -print -delete
+fi
