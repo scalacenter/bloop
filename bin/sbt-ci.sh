5a004790d2e3d911e2c84b55d7313d41fa300844
Jorge Vicente Cantero <jorgevc@fastmail.es>
2018-07-24 00:06:45 +0200

    Fix non-existing grep's --silent in Drone
    Agrrr.

--- bin/sbt-ci.sh
+++ bin/sbt-ci.sh
@@ -2,7 +2,7 @@
 set -o nounset
 
 # I don't trust $DRONE_COMMIT_MESSAGE here, it's had problems in the future
-if git log -1 --pretty=%B | grep DOCS --silent; then
+if git log -1 --pretty=%B | grep DOCS > /dev/null 2>&1; then
   echo "Ignoring sbt invocation, docs change was detected so we only update the docs site."
   sbt -sbt-dir /drone/.sbt/1.0 -sbt-boot /drone/.sbt/boot docs/makeSite
 else
