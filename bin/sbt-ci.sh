09bf35d790a0a72bbbdbfc61085a6d424af69b55
Jorge Vicente Cantero <jorgevc@fastmail.es>
2018-07-24 10:22:45 +0200

    Try again if it works [DOCS]
    In both windows and Linux.

--- bin/sbt-ci.sh
+++ bin/sbt-ci.sh
@@ -3,7 +3,7 @@ set -o nounset
 
 # I don't trust $DRONE_COMMIT_MESSAGE here, it's had problems in the future
 if git log -1 --pretty=%B --no-merges | grep DOCS > /dev/null 2>&1; then
-  echo "Ignoring sbt invocation, docs change was detected so we only update the docs site."
+  echo "Found DOCS in commit message, the CI will only update the docs site."
   sbt -sbt-dir /drone/.sbt/1.0 -sbt-boot /drone/.sbt/boot docs/makeSite
 else
   sbt -sbt-dir /drone/.sbt/1.0 -sbt-boot /drone/.sbt/boot $@
