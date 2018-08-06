ef73784f3612eefa98946a188e9ebead8e841026
Jorge Vicente Cantero <jorgevc@fastmail.es>
2018-08-06 23:57:39 +0200

    Remove ghpages corrupt dir [DOCS]

--- bin/sbt-ci.sh
+++ bin/sbt-ci.sh
@@ -6,10 +6,14 @@ set -o nounset
 if git log -1 --pretty=%B --no-merges | grep DOCS > /dev/null 2>&1; then
   echo "Found DOCS in commit message, the CI will only update the docs site."
   if [[ "$DRONE_BUILD_EVENT" != "pull_request" && "$DRONE_BRANCH" == "master" ]]; then
+    echo "Updating website with the docs (tag or push to master detected)"
+    ls -l /drone/.sbt/ghpages
+    rm -rf /drone/.sbt/ghpages
     if ! sbt -sbt-dir /drone/.sbt/1.0 -sbt-boot /drone/.sbt/boot docs/ghpagesPushSite; then
       exit 1
     fi
   else 
+    echo "Making docs... (pull request detected)"
     # If publish docs is not set, just build them
     if ! sbt -sbt-dir /drone/.sbt/1.0 -sbt-boot /drone/.sbt/boot docs/makeSite; then
       exit 1
