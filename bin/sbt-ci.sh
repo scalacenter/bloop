a232638d7a4891b4743b91bec645e6b13c0f21dd
Jorge Vicente Cantero <jorgevc@fastmail.es>
2018-08-06 22:46:26 +0200

    Fix CI docs push

--- bin/sbt-ci.sh
+++ bin/sbt-ci.sh
@@ -4,11 +4,12 @@ set -o nounset
 # I don't trust $DRONE_COMMIT_MESSAGE here, it's had problems in the future
 if git log -1 --pretty=%B --no-merges | grep DOCS > /dev/null 2>&1; then
   echo "Found DOCS in commit message, the CI will only update the docs site."
-  if [[ "$DRONE_BUILD_EVENT" != "pull_request" && "$DRONE_BRANCH" == "master" ]]; then
-    if ! sbt -sbt-dir /drone/.sbt/1.0 -sbt-boot /drone/.sbt/boot docs/makeSite docs/ghpagesPushSite; then
+  if [[ ! -z "PUBLISH_DOCS" ]]; then
+    if ! sbt -sbt-dir /drone/.sbt/1.0 -sbt-boot /drone/.sbt/boot docs/ghpagesPushSite; then
       exit 1
     fi
   else 
+    # If publish docs is not set, just build them
     if ! sbt -sbt-dir /drone/.sbt/1.0 -sbt-boot /drone/.sbt/boot docs/makeSite; then
       exit 1
     fi
