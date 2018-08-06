34b05b257eeac8de6b4ec8a4338e2386f0ec996a
Jorge Vicente Cantero <jorgevc@fastmail.es>
2018-08-06 22:14:43 +0200

    Push docs when only [DOCS] is used

--- bin/sbt-ci.sh
+++ bin/sbt-ci.sh
@@ -4,8 +4,14 @@ set -o nounset
 # I don't trust $DRONE_COMMIT_MESSAGE here, it's had problems in the future
 if git log -1 --pretty=%B --no-merges | grep DOCS > /dev/null 2>&1; then
   echo "Found DOCS in commit message, the CI will only update the docs site."
-  if ! sbt -sbt-dir /drone/.sbt/1.0 -sbt-boot /drone/.sbt/boot docs/makeSite; then
-    exit 1
+  if [[ "$DRONE_BUILD_EVENT" != "pull_request" && "$DRONE_BRANCH" == "master" ]]; then
+    if ! sbt -sbt-dir /drone/.sbt/1.0 -sbt-boot /drone/.sbt/boot docs/makeSite docs/ghpagesPushSite; then
+      exit 1
+    fi
+  else 
+    if ! sbt -sbt-dir /drone/.sbt/1.0 -sbt-boot /drone/.sbt/boot docs/makeSite; then
+      exit 1
+    fi
   fi
 else
   if ! sbt -sbt-dir /drone/.sbt/1.0 -sbt-boot /drone/.sbt/boot "$@"; then
