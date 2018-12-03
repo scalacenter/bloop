6d0d7aafcdeb7e327cc7f9f48c500655cb14a0bc
jvican <jorgevc@fastmail.es>
2018-12-03 11:08:54 +0100

    updated site

--- docson/lib/server.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const path = require('path');
-const app = express();
-
-app.use(express.static('files'))
-
-app.use('/', express.static(path.join(__dirname, '..')) );
-
-module.exports = app;
--- docson/scripts/server.js
+++
@@ -1,4 +0,0 @@
-require('../lib/server').listen(
-    3000,
-    () => console.log( "server running on port 3000")
-);
