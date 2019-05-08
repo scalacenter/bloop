dd9e9dd33801bb7ffac5c78d7c05e186bd627f6f
Jorge Vicente Cantero <jorgevc@fastmail.es>
2019-05-09 00:02:24 +0200

    Use right path to project dir in sbt benchmark

--- benchmarks/src/main/scala/sbt/HotSbtBenchmark.scala
+++ benchmarks/src/main/scala/sbt/HotSbtBenchmark.scala
@@ -75,7 +75,7 @@ class HotSbtBenchmark {
       else s"${projectName}/compile"
     }
 
-    path = CommunityBuild.getConfigDirForBenchmark(project).getParent.getParent
+    path = CommunityBuild.getConfigDirForBenchmark(project).getParent
     cleanClassesPath = path.resolve("project").resolve("CleanClassesPlugin.scala")
     Files.write(cleanClassesPath, cleanClassesPlugin.getBytes("UTF-8"))
     val sbtLaucherPath = System.getProperty("sbt.launcher")
