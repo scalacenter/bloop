0e3fa992150d2598e16c404fa42f3c5aa4f777ea
Jorge Vicente Cantero <jorgevc@fastmail.es>
2019-05-09 22:40:35 +0200

    Update python installation script to latest coursier

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -12,7 +12,7 @@ object Dependencies {
   val zincVersion = "1.2.1+110-85b9a03c"
   val bspVersion = "2.0.0-M1"
   val scalazVersion = "7.2.20"
-  val coursierVersion = "1.1.0-M8"
+  val coursierVersion = "1.1.0-M14-4"
   val lmVersion = "1.0.0"
   val configDirsVersion = "10"
   val caseAppVersion = "1.2.0-faster-compile-time"
