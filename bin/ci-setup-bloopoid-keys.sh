8d14a3c3bdcdba63ed4db5f23632c96e48bb790d
Jorge Vicente Cantero <jorgevc@fastmail.es>
2019-06-06 21:49:45 +0200

    Do export environment variable [DOCS]
    This made the release step fail, we add this so that the next release
    step doesn't fail.

--- bin/ci-setup-bloopoid-keys.sh
+++ bin/ci-setup-bloopoid-keys.sh
@@ -6,7 +6,7 @@ git config --global user.email "bloopoid@trashmail.ws"
 
 # Setup everything to be able to push to github pages
 mkdir -p ~/.ssh
-BLOOPOID_PRIVATE_KEY_PATH="/root/.ssh/id_rsa"
+export BLOOPOID_PRIVATE_KEY_PATH="/root/.ssh/id_rsa"
 echo -e "Host github.com\\n\\tStrictHostKeyChecking no\\n" > ~/.ssh/config
 echo -e "${BLOOPOID_PRIVATE_KEY}\\n" > ~/.ssh/id_rsa
 chmod 0600 ~/.ssh/id_rsa
