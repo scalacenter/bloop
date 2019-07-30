7ff9cb3b933926394d06cd5ba4613c61a105f85b
jvican <jorgevc@fastmail.es>
2019-07-30 17:57:37 +0200

    Remove loaded build abstraction

--- frontend/src/main/scala/bloop/data/LoadedBuild.scala
+++
@@ -1,9 +0,0 @@
-package bloop.data
-
-/**
- * A partial loaded build is the incremental result of loading a certain amount
- * of configuration files from disk and post-processing them in-memory.
- */
-case class PartialLoadedBuild(
-    projects: List[LoadedProject]
-)
