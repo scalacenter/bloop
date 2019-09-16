5fca109978f4e7cd6ba4408675406e7639755998
jvican <jorgevc@fastmail.es>
2019-09-16 12:50:09 +0200

    Fix matching on `classes-empty-` in `BloopLookup`

--- backend/src/main/scala/sbt/internal/inc/bloop/internal/BloopLookup.scala
+++ backend/src/main/scala/sbt/internal/inc/bloop/internal/BloopLookup.scala
@@ -14,12 +14,27 @@ final class BloopLookup(
   implicit val filter: DebugFilter = DebugFilter.Compilation
   private val classpathHash: Vector[FileHash] =
     compileConfiguration.currentSetup.options.classpathHash.toVector
+  private val ClassesEmptyDirPrefix = java.io.File.separator + "classes-empty-"
   override def changedClasspathHash: Option[Vector[FileHash]] = {
     if (classpathHash == previousClasspathHash) None
     else {
       // Discard directories and known empty classes dirs and check if there's still a hash mismatch
-      val newPreviousClasspathHash = previousClasspathHash
-        .filterNot(fh => fh.file.isDirectory() || fh.file.getName().startsWith("classes-empty-"))
+      val newPreviousClasspathHash = previousClasspathHash.filterNot { fh =>
+        // If directory exists, filter it out
+        fh.file.isDirectory() ||
+        /* Empty classes dirs don't exist so match on path.
+         *
+         * Don't match on `getFileName` because `classes-empty` is followed by
+         * target name, which could contains `java.io.File.separator`, making
+         * `getFileName` pick the suffix after the latest separator.
+         *
+         * e.g. if target name is
+         * `util/util-function/src/main/java/com/twitter/function:function`
+         * classes empty dir path will be
+         * `classes-empty-util/util-function/src/main/java/com/twitter/function:function`.
+         */
+        fh.file.getAbsolutePath.contains(ClassesEmptyDirPrefix)
+      }
 
       if (classpathHash == newPreviousClasspathHash) None
       else {
