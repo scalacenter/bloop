3401581faf2059009f2c38e0821f88027d25af6c
Olafur Pall Geirsson <lgeirsson@twitter.com>
2020-03-27 15:00:05 +0000

    Use `BuildInfo.version` for GlobalSettingsSpec

--- launcher/src/test/scala/bloop/launcher/GlobalSettingsSpec.scala
+++ launcher/src/test/scala/bloop/launcher/GlobalSettingsSpec.scala
@@ -5,7 +5,7 @@ import scala.concurrent.Promise
 import bloop.bloopgun.util.Environment
 import bloop.internal.build.BuildInfo
 
-object GlobalSettingsSpec extends LauncherBaseSuite("1.3.2", BuildInfo.bspVersion, 9014) {
+object GlobalSettingsSpec extends LauncherBaseSuite(BuildInfo.version, BuildInfo.bspVersion, 9014) {
   val launcherArguments = Array(bloopVersion, "--skip-bsp-connection")
 
   def writeJsonSettings(json: String): Unit = {
