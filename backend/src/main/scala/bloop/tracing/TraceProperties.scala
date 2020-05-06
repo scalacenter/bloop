13494e8dadd4f7cbd528f944191ed0220289a740
Win Wang <winw@twitter.com>
2020-05-06 19:04:17 -0400

    Fix typo in debugTracing property

--- backend/src/main/scala/bloop/tracing/TraceProperties.scala
+++ backend/src/main/scala/bloop/tracing/TraceProperties.scala
@@ -14,7 +14,7 @@ case class TraceProperties(
 object TraceProperties {
   val default: TraceProperties = {
     val verbose = Properties.propOrFalse("bloop.tracing.verbose")
-    val debugTracing = Properties.propOrFalse("bloop.tracing.debug")
+    val debugTracing = Properties.propOrFalse("bloop.tracing.debugTracing")
     val localServiceName = Properties.propOrElse("bloop.tracing.localServiceName", "bloop")
     val traceStartAnnotation = Properties.propOrNone("bloop.tracing.traceStartAnnotation")
     val traceEndAnnotation = Properties.propOrNone("bloop.tracing.traceEndAnnotation")
