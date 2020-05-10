2bf5ffc5aa4a6a427e5340c99e86a9c3e6b77965
Jorge Vicente Cantero <jorgevc@fastmail.es>
2020-05-11 01:32:02 +0200

    Fix GITHUB_REF *again*

--- bin/sbt-ci-release.sh
+++ bin/sbt-ci-release.sh
@@ -1,8 +1,7 @@
 #!/usr/bin/env bash
 set -o nounset
 
-echo "$GITHUB_REF"
-if [[ "$GITHUB_REF" = "/refs/tags/"* ]]; then
+if [[ "$GITHUB_REF" = "refs/tags/"* ]]; then
   if sbt "$@"; then
     exit 1
   fi
