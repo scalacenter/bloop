31275524f87b339d910bac6e3d2ea536fad35d43
Jorge Vicente Cantero <jorgevc@fastmail.es>
2020-05-11 02:45:21 +0200

    Source env variables for BLOOPOID_SSH_PRIVATE_KEY correctly

--- bin/ci-setup-bloopoid-keys.sh
+++ bin/ci-setup-bloopoid-keys.sh
@@ -6,7 +6,9 @@ git config --global user.email "bloopoid@trashmail.ws"
 
 # Setup everything to be able to push to github pages
 mkdir -p ~/.ssh
-export BLOOPOID_PRIVATE_KEY_PATH="/root/.ssh/id_rsa"
 echo -e "Host github.com\\n\\tStrictHostKeyChecking no\\n" > ~/.ssh/config
-echo -e "${BLOOPOID_PRIVATE_KEY}\\n" > ~/.ssh/id_rsa
+echo -e "${BLOOPOID_SSH_PRIVATE_KEY}\\n" > ~/.ssh/id_rsa
+echo -e "${BLOOPOID_SSH_PUBLIC_KEY}\\n" > ~/.ssh/id_rsa.pub
 chmod 0600 ~/.ssh/id_rsa
+chmod 0600 ~/.ssh/id_rsa.pub
+export BLOOPOID_PRIVATE_KEY_PATH="/root/.ssh/id_rsa"
