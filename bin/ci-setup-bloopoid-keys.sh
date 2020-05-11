3a89b8a92200be9160ac81b356cf6e67f11fb4f6
Jorge Vicente Cantero <jorgevc@fastmail.es>
2020-05-11 11:06:40 +0200

    Don't set bloopoid private key to `/root/`

--- bin/ci-setup-bloopoid-keys.sh
+++ bin/ci-setup-bloopoid-keys.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
+set -o nounset
+set -eux
 
 # Add git metadata for the push to succeed
 git config --global user.name "Bloopoid"
@@ -6,9 +8,9 @@ git config --global user.email "bloopoid@trashmail.ws"
 
 # Setup everything to be able to push to github pages
 mkdir -p ~/.ssh
+export BLOOPOID_PRIVATE_KEY_PATH="$HOME/.ssh/id_rsa"
 echo -e "Host github.com\\n\\tStrictHostKeyChecking no\\n" > ~/.ssh/config
 echo -e "${BLOOPOID_SSH_PRIVATE_KEY}\\n" > ~/.ssh/id_rsa
 echo -e "${BLOOPOID_SSH_PUBLIC_KEY}\\n" > ~/.ssh/id_rsa.pub
 chmod 0600 ~/.ssh/id_rsa
 chmod 0600 ~/.ssh/id_rsa.pub
-export BLOOPOID_PRIVATE_KEY_PATH="/root/.ssh/id_rsa"
