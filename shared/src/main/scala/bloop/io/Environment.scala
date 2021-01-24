7628339028beae92a4a2ede70b0a4f6060116e6c
Phil <philwalk9@gmail.com>
2021-01-24 15:05:59 -0700

    cleanup and document io.Environment

--- shared/src/main/scala/bloop/io/Environment.scala
+++ shared/src/main/scala/bloop/io/Environment.scala
@@ -2,67 +2,87 @@ package bloop.io
 
 object Environment {
 
-  def isWindows: Boolean = scala.util.Properties.isWin
-
-  // value of system environment variable OSTYPE
-  def ostype: String = getenvOpt("OSTYPE").getOrElse("")
-
-  /*
-   * environment variable SHELL defined by default in CYGWIN, MinGW, msys, Git Bash.
+  /**
+   * Returns "\n" if both OSTYPE and SHELL are nonEmpty, returns `System.lineSeparator` otherwise.
+   * @see [[shellLineSeparator]] which is similar.
    */
-  def shell: String = getenvOpt("SHELL").getOrElse("")
+  def lineSeparator: String = if (validOstype.nonEmpty) shellLineSeparator else System.lineSeparator
 
-  /*
-   * Validated shell name, or empty string.
+  /**
+   * Returns preferred SHELL environment lineSeparator:
+   *    '\r\n' on Windows, unless SHELL environment is recognized.
+   *    '\n'   everywhere else
+   *
+   * The default ending in Windows is '\r\n', but either of two conditions can
+   * change the value of [[shellLineSeparator]]:
+   *    1. if [[validShell]] is nonEmpty
+   *    2. if system property `"line.separator"` is modified
+   *
+   * Unlike [[lineSeparator]], only depends on the value of SHELL.
+   *
+   * @return preferred lineSeparator for SHELL environments, or `System.lineSeparator` otherwise.
    */
-  def validShell: String = validShells.find { _ == shell } match {
+  def shellLineSeparator: String = if (validShell.nonEmpty) "\n" else sysLineSeparator
+
+  /**
+   * Preferred line ending for SHELL environments.
+   *
+   * An alias for `shellLineSeparator`.
+   *
+   * @example
+   * {{{
+   *   print(someText + EOL)
+   * }}}
+   */
+  lazy val EOL: String = shellLineSeparator // alias for shellLineSeparator
+
+  /**
+   * Return the canonical lowercased shell name, or empty string.
+   *
+   * `shell` is valid if when lowercased, it has a `validShells` entry as substring.
+   *
+   * @return the matching `validShells` entry, if there is a match; empty string otherwise.
+   *
+   * @example if {{{SHELL=/usr/bin/bash}}}, returns {{{/bin/bash}}}.
+   *
+   */
+  def validShell: String = validShells.find { _.endsWith(shellLC) } match {
     case Some(sh) => sh
     case _ => ""
   }
 
   /*
-   * The preferred lineSeparator for SHELL environments.
+   * Return lowercase shell with possible .exe extension removed.
    */
-  def lineSeparator: String = if (validShell.nonEmpty) "\n" else sysLineSeparator
-
-  /*
-   * NOTE: defining System property "line.separator" does not seem to
-   * override the value of System.lineSeparator, so this is preferred to
-   * permit programmability at jvm launch time.
-   *
-   * System properties may be redefined on JVM startup, e.g.:
-   *     <pre>
-   *     $ bloop -Dline.separator=$'\n' server
-   *     </pre>
-   *
-   * This should not be used in Windows unless validShell == "".
-   */
-  private def sysLineSeparator: String = propsOpt("line.separator").getOrElse("\n")
-
-  /*
-   * Validated, lowercased and abbreviated OSTYPE value, or empty string.
-   */
-  lazy val validOstype: String = {
-    ostype match {
-      case "" =>
-        ""
-      case os =>
-        val lcos = os.toLowerCase(java.util.Locale.ENGLISH)
-        validOstypes.find { lcos.contains(_) } match {
-          case Some(os) =>
-            os // lowercase abbreviated value
-          case _ =>
-            ""
-        }
-    }
+  private[this] def shellLC = shell.toLowerCase(java.util.Locale.ENGLISH) match {
+    case sh if sh.endsWith(".exe") => sh.replaceFirst(".exe$", "")
+    case sh => sh
   }
 
   /*
-   * EOL is:
-   *    '\r\n' on Windows, unless SHELL is a unix-like shell environment
-   *    '\n'   everywhere else
+   * Return the name of the SHELL environment variable, or empty string.
+   *
+   * Normally set in CYGWIN, MinGW, msys, Git Bash shell environments.
    */
-  lazy val EOL: String = lineSeparator
+  lazy val shell: String = getenvOpt("SHELL").getOrElse("")
+
+  /*
+   * Returns current value of system property `"line.separator"`.
+   *
+   * System.lineSeparator may only be redefined at JVM startup,
+   * whereas this may be redefined on the fly in client code.
+   *
+   * System.lineSeparator always returns the initial value of system property "line.separator".
+   * @see [[https://docs.oracle.com/javase/8/docs/api/java/lang/System.html#lineSeparator--]]
+   *
+   * System properties may be redefined on JVM startup, e.g., from a shell environment:
+   * @example
+   *  {{{
+   *  $ java -J-Dline.separator=$'\n' ...
+   *  }}}
+   *
+   */
+  def sysLineSeparator: String = propsOpt("line.separator").getOrElse("\n")
 
   def propsOpt(propname: String): Option[String] = Option(sys.props(propname))
 
@@ -77,31 +97,32 @@ object Environment {
    */
   def lineSplit(str: String): Array[String] = str.split(END_OF_LINE_MATCHER, -1)
 
-  /*
-   * Add extension method to String for reliable line splitting.
+  /**
+   * Extend String for reliable line splitting.
+   *
+   * @example
+   * {{{
+   *   val lines = text.splitLines // equivalent to `text.split(END_OF_LINE_MATCHER)`
+   * }}}
+   *
    */
   implicit class LineSplitter(str: String) {
     def splitLines: Array[String] = str.split(END_OF_LINE_MATCHER, -1)
   }
 
-  /*
-   * reliably split strings to lines, regardless of where they originated
-   * borrowed from zinc DiagnosticsReporter.scala
+  /**
+   * Regular expression that reliably splits Strings into lines.
+   *
+   * Effective for all commonly encountered line endings:
+   *  - "\n"
+   *  - "\r\n"
+   *  - "\r"
    */
-  def END_OF_LINE_MATCHER = "\r\n|\r|\n"
+  def END_OF_LINE_MATCHER = "\n|\r\n|\r"
 
   /*
-   * A list of valid ostype substrings.  The lower-case OSTYPE environment
-   * variable must contain one of these substrings to be supported.
-   */
-  lazy val validOstypes = Seq(
-    "cygwin",
-    "msys",
-    "mingw"
-  )
-  /*
    * A list of valid shell paths.  The SHELL environment variable must
-   * match one of these paths to be considered a valid shell.
+   * have one of these entries as a substring to be considered a valid shell.
    */
   lazy val validShells = Seq(
     "/bin/sh",
@@ -112,15 +133,42 @@ object Environment {
     "/bin/pdksh",
     "/bin/posh",
     "/bin/tcsh",
-    "/bin/zsh",
-    "/usr/bin/sh",
-    "/usr/bin/ash",
-    "/usr/bin/bash",
-    "/usr/bin/dash",
-    "/usr/bin/mksh",
-    "/usr/bin/pdksh",
-    "/usr/bin/posh",
-    "/usr/bin/tcsh",
-    "/usr/bin/zsh"
+    "/bin/zsh"
   )
+
+  /*
+   * Returns the empty String unless OSTYPE is defined.
+   */
+  lazy val ostype: String = getenvOpt("OSTYPE").getOrElse("")
+
+  /*
+   * Returns validated, lowercase abbreviated OSTYPE value, or empty string.
+   *
+   * Replaced by the entry of [[validOsTypes]] having [[ostype]] as a substring.
+   *
+   */
+  def validOstype: String = {
+    ostype.toLowerCase(java.util.Locale.ENGLISH) match {
+      case "" => ""
+      case os =>
+        validOstypes.find { os.contains(_) }.getOrElse("")
+    }
+  }
+
+  /*
+   * A list of valid ostype substrings.  The lower-case OSTYPE environment
+   * variable must contain one of these substrings to be supported.
+   */
+  lazy val validOstypes = Seq(
+    "cygwin",
+    "msys",
+    "mingw",
+    "linux",
+    "darwin",
+    "osx",
+    "sunos",
+    "solaris",
+    "aix"
+  )
+
 }
