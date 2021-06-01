70161485f06a89daba767a684344fa2bff4f6c79
Tomasz Godzik <tomek.godzik@gmail.com>
2021-06-01 08:44:30 +0200

    Update nuprocess to 2.0.2 to support Apple M1

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -41,7 +41,7 @@ object Dependencies {
   val circeVersion = "0.9.3"
   val jsoniterVersion = "2.4.0"
   val circeVersion213 = "0.12.2"
-  val nuprocessVersion = "2.0.1"
+  val nuprocessVersion = "2.0.2"
   val shapelessVersion = "2.3.4"
   val scalaNative03Version = "0.3.9"
   val scalaNative04Version = "0.4.0"
