00ae949313a2cfa0425c98808f857a5428e1603e
Alexandre Archambault <alexarchambault@users.noreply.github.com>
2022-01-05 02:12:38 +0100

    Use coursier/setup-scala (#42)

--- bin/sbt-ci.sh
+++ bin/sbt-ci.sh
@@ -3,11 +3,11 @@ set -o nounset
 
 if git log -1 --pretty=%B --no-merges | grep DOCS > /dev/null 2>&1; then
   echo "Found DOCS in commit message, the CI will only build the docs site."
-  if ! sbtx docs/run; then
+  if ! sbt docs/run; then
     exit 1
   fi
 else
-  if ! sbtx "$@"; then
+  if ! sbt "$@"; then
     exit 1
   fi
 fi
