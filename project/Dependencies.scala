4f11882b8b7b872b951ee54f15641454a789d059
Adrien Piquerez <adrien.piquerez@gmail.com>
2022-02-11 10:56:47 +0100

    Bump debug adapter to 2.0.13

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -55,7 +55,7 @@ object Dependencies {
   val asmVersion = "7.0"
   val snailgunVersion = "0.4.0"
   val ztExecVersion = "1.11"
-  val debugAdapterVersion = "2.0.12"
+  val debugAdapterVersion = "2.0.13"
 
   import sbt.librarymanagement.syntax.stringToOrganization
   val zinc = "ch.epfl.scala" %% "zinc" % zincVersion
