2d530736fdb63c34df35112c172dd0b68087db0a
Tomasz Godzik <tgodzik@virtuslab.com>
2022-02-21 17:21:54 +0100

    Rename terms according to the Scala language guide
    Based on https://www.scala-lang.org/contribute/inclusive-language/

--- frontend/src/test/resources/compiler-plugin-allowlist/sandbox/src/main/scala/hello/App.scala
+++ frontend/src/test/resources/compiler-plugin-allowlist/sandbox/src/main/scala/hello/App.scala
@@ -2,6 +2,6 @@ package hello
 
 object App {
   def main(args: Array[String]): Unit = {
-    println("Whitelist application was compiled successfully v3.0")
+    println("Allowed application was compiled successfully")
   }
 }
