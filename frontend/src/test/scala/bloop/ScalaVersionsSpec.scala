e1c21fec58ec556387f671f619ffc391b2d1816e
Anton Sviridov <keynmol@gmail.com>
2022-07-01 14:15:12 +0100

    Use latest Scala 2 and Scala 3 versions in tests

--- frontend/src/test/scala/bloop/ScalaVersionsSpec.scala
+++ frontend/src/test/scala/bloop/ScalaVersionsSpec.scala
@@ -12,70 +12,75 @@ import monix.eval.Task
 import monix.execution.misc.NonFatal
 
 object ScalaVersionsSpec extends bloop.testing.BaseSuite {
-  test("cross-compile build to latest Scala versions") {
-    var loggers: List[RecordingLogger] = Nil
-    def compileProjectFor(scalaVersion: String): Task[Unit] = Task {
-      TestUtil.withinWorkspace { workspace =>
-        val (compilerOrg, compilerArtifact) = {
-          if (scalaVersion.startsWith("3.")) "org.scala-lang" -> "scala3-compiler_3.0.0-M3"
-          else "org.scala-lang" -> "scala-compiler"
-        }
+  var loggers: List[RecordingLogger] = Nil
 
-        def jarsForScalaVersion(version: String, logger: RecordingLogger) = {
-          ScalaInstance
-            .resolve(compilerOrg, compilerArtifact, version, logger)
-            .allJars
-            .map(AbsolutePath(_))
-        }
-
-        val source = {
-          if (compilerArtifact.contains("scala3-compiler")) {
-            """/main/scala/Foo.scala
-              |class Foo { val x: String | Int = 1 }
-            """.stripMargin
-          } else {
-            """/main/scala/Foo.scala
-              |class Foo
-            """.stripMargin
-          }
-        }
-
-        val logger = new RecordingLogger(ansiCodesSupported = false)
-        loggers.synchronized { loggers = logger :: loggers }
-        val jars = jarsForScalaVersion(scalaVersion, logger)
-        val `A` = TestProject(
-          workspace,
-          "a",
-          List(source),
-          scalaOrg = Some(compilerOrg),
-          scalaCompiler = Some(compilerArtifact),
-          scalaVersion = Some(scalaVersion),
-          jars = jars
-        )
-
-        val projects = List(`A`)
-        val state = loadState(workspace, projects, logger)
-        val compiledState = state.compile(`A`)
-        try {
-          Predef.assert(compiledState.status == ExitStatus.Ok)
-          assertValidCompilationState(compiledState, projects)
-        } finally loggers.synchronized { loggers = loggers.filterNot(_ == logger) }
+  def compileProjectFor(scalaVersion: String): Task[Unit] = Task {
+    TestUtil.withinWorkspace { workspace =>
+      val (compilerOrg, compilerArtifact) = {
+        if (scalaVersion.startsWith("3."))
+          "org.scala-lang" -> "scala3-compiler_3"
+        else "org.scala-lang" -> "scala-compiler"
       }
-    }
 
-    val `2.10` = compileProjectFor("2.10.7")
-    val `2.11` = compileProjectFor("2.11.12")
-    val `2.12` = compileProjectFor("2.12.9")
-    val `2.13` = compileProjectFor("2.13.2")
-    val `2.13.3` = compileProjectFor("2.13.3")
-    val `2.13.7` = compileProjectFor("2.13.7")
-    val `2.13.8` = compileProjectFor("2.13.8")
-    val LatestDotty = compileProjectFor("3.0.0-M3")
-    val all = {
-      if (TestUtil.isJdk8)
-        List(`2.10`, `2.11`, `2.12`, `2.13`, `2.13.3`, `2.13.7`, `2.13.8`, LatestDotty)
-      else List(`2.12`, `2.13`, `2.13.3`, `2.13.7`)
+      def jarsForScalaVersion(version: String, logger: RecordingLogger) = {
+        ScalaInstance
+          .resolve(compilerOrg, compilerArtifact, version, logger)
+          .allJars
+          .map(AbsolutePath(_))
+      }
+
+      val source = {
+        if (compilerArtifact.contains("scala3-compiler")) {
+          """/main/scala/Foo.scala
+            |class Foo { val x: String | Int = 1 }
+            """.stripMargin
+        } else {
+          """/main/scala/Foo.scala
+            |class Foo
+            """.stripMargin
+        }
+      }
+
+      val logger = new RecordingLogger(ansiCodesSupported = false)
+      loggers.synchronized { loggers = logger :: loggers }
+      val jars = jarsForScalaVersion(scalaVersion, logger)
+      val `A` = TestProject(
+        workspace,
+        "a",
+        List(source),
+        scalaOrg = Some(compilerOrg),
+        scalaCompiler = Some(compilerArtifact),
+        scalaVersion = Some(scalaVersion),
+        jars = jars
+      )
+
+      val projects = List(`A`)
+      val state = loadState(workspace, projects, logger)
+      val compiledState = state.compile(`A`)
+      try {
+        Predef.assert(compiledState.status == ExitStatus.Ok)
+        assertValidCompilationState(compiledState, projects)
+      } finally loggers.synchronized { loggers = loggers.filterNot(_ == logger) }
     }
+  }
+
+  val jdk8OnlyVersions = List(
+    "2.10.7",
+    "2.11.12"
+  )
+  val scalaVersions = List(
+    "2.12.16",
+    "2.13.6",
+    "2.13.7",
+    "2.13.8",
+    "3.0.2",
+    "3.1.3"
+  )
+
+  val allVersions = if (TestUtil.isJdk8) jdk8OnlyVersions ++ scalaVersions else scalaVersions
+
+  test("cross-compile build to latest Scala versions") {
+    val all = allVersions.map(compileProjectFor)
 
     try {
       TestUtil.await(FiniteDuration(120, "s"), ExecutionContext.ioScheduler) {
