5380f9edbcfbb0a72e6421e1e0be104584a487b9
Tomasz Godzik <tgodzik@virtuslab.com>
2022-09-29 17:51:51 +0200

    bugfix: Fix running tests

--- frontend/src/test/scala/bloop/bsp/BspTestSpec.scala
+++ frontend/src/test/scala/bloop/bsp/BspTestSpec.scala
@@ -6,16 +6,16 @@ import bloop.io.AbsolutePath
 import bloop.logging.RecordingLogger
 import bloop.util.TestProject
 import bloop.util.TestUtil
+import bloop.internal.build.BuildTestInfo
+import bloop.ScalaInstance
 
 object TcpBspTestSpec extends BspTestSpec(BspProtocol.Tcp)
 object LocalBspTestSpec extends BspTestSpec(BspProtocol.Local)
 
 class BspTestSpec(override val protocol: BspProtocol) extends BspBaseSuite {
 
-  val junitJars = System
-    .getProperty("java.class.path")
-    .split(java.io.File.pathSeparator)
-    .collect { case path if path.contains("junit") => AbsolutePath(path) }
+  val junitJars = BuildTestInfo.junitTestJars.map(AbsolutePath.apply)
+  private val scalaVersion = "2.12.15"
 
   test("bsp test succeeds") {
     TestUtil.withinWorkspace { workspace =>
@@ -29,9 +29,16 @@ class BspTestSpec(override val protocol: BspProtocol) extends BspBaseSuite {
 
       val logger = new RecordingLogger(ansiCodesSupported = false)
 
-      val A = TestProject(workspace, "a", sources, jars = junitJars)
+      val compilerJars = ScalaInstance
+        .resolve("org.scala-lang", "scala-compiler", scalaVersion, logger)
+        .allJars
+        .map(AbsolutePath.apply)
+
+      val A =
+        TestProject(workspace, "a", sources, enableTests = true, jars = compilerJars ++ junitJars)
       loadBspState(workspace, List(A), logger) { state =>
-        val testResult: ManagedBspTestState = state.test(A)
+        val compiled = state.compile(A)
+        val testResult = compiled.toTestState.test(A)
         assertExitStatus(testResult, ExitStatus.Ok)
       }
     }
@@ -40,18 +47,26 @@ class BspTestSpec(override val protocol: BspProtocol) extends BspBaseSuite {
   test("bsp test fails") {
     TestUtil.withinWorkspace { workspace =>
       val sources = List(
-        """/main/scala/FooTest.scala
+        """/FooTest.scala
           |class FooTest {
-          |  @org.junit.Test def foo(): Unit = org.junit.Assert.fail()
+          |  @org.junit.Test def foo(): Unit = {
+          |    org.junit.Assert.fail()
+          |  }
           |}
           """.stripMargin
       )
 
       val logger = new RecordingLogger(ansiCodesSupported = false)
+      val compilerJars = ScalaInstance
+        .resolve("org.scala-lang", "scala-compiler", scalaVersion, logger)
+        .allJars
+        .map(AbsolutePath.apply)
 
-      val A = TestProject(workspace, "a", sources, jars = junitJars)
+      val A =
+        TestProject(workspace, "a", sources, enableTests = true, jars = compilerJars ++ junitJars)
       loadBspState(workspace, List(A), logger) { state =>
-        val testResult: ManagedBspTestState = state.test(A)
+        val compiled = state.compile(A)
+        val testResult = compiled.toTestState.test(A)
         assertExitStatus(testResult, ExitStatus.TestExecutionError)
       }
     }
