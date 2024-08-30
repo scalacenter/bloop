5757dd89837888dfa02ccfe43040a54746d9696a
Jan Chyb <jan.chyb.99@gmail.com>
2024-08-30 18:21:53 +0200

    Delete betasty in newClassesDir, completely restart BE compilations after an error

--- frontend/src/main/scala/bloop/engine/tasks/compilation/CompileGraph.scala
+++ frontend/src/main/scala/bloop/engine/tasks/compilation/CompileGraph.scala
@@ -449,7 +449,7 @@ object CompileGraph {
                           .+=(newProducts.readOnlyClassesDir.toFile -> newResult)
                       case (p, ResultBundle(f: Compiler.Result.Failed, _, _, _)) =>
                         f.bestEffortProducts.foreach {
-                          case BestEffortProducts(products, _) =>
+                          case BestEffortProducts(products, _, _) =>
                             dependentProducts += (p -> Right(products))
                         }
                       case _ => ()
