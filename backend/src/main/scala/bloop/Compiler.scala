46a5bd9558bb2b482dd7b244db631535ebd4b5f3
Jan Chyb <jan.chyb.99@gmail.com>
2024-09-23 13:28:30 +0200

    Run OrganiseImports

--- backend/src/main/scala/bloop/Compiler.scala
+++ backend/src/main/scala/bloop/Compiler.scala
@@ -5,6 +5,7 @@ import java.io.PrintWriter
 import java.io.StringWriter
 import java.nio.file.Files
 import java.nio.file.Path
+import java.nio.file.Paths
 import java.util.Optional
 import java.util.concurrent.Executor
 
