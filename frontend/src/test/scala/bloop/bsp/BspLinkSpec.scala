bda89a6f69d46850e9aa819418d85062c5fff38d
Kristian Nedrevold-Hansen <kristian@krined.no>
2024-12-18 12:53:53 +0100

    Update BspLinkSpec to check if JS file has been created

--- frontend/src/test/scala/bloop/bsp/BspLinkSpec.scala
+++ frontend/src/test/scala/bloop/bsp/BspLinkSpec.scala
@@ -3,12 +3,12 @@ package bloop.bsp
 import bloop.cli.BspProtocol
 import bloop.util.TestUtil
 import bloop.logging.RecordingLogger
-import bloop.cli.ExitStatus
 import bloop.util.TestProject
 import bloop.data.Project
 import bloop.data.Platform
 import bloop.engine.tasks.toolchains.ScalaJsToolchain
 import bloop.engine.State
+import bloop.cli.ExitStatus
 
 object TcpBspLinkSpec extends BspLinkSpec(BspProtocol.Tcp)
 object LocalBspLinkSpec extends BspLinkSpec(BspProtocol.Local)
@@ -40,7 +40,13 @@ class BspLinkSpec(
         val newB = build.copy(state = build.state.copy(state = state0))
         val compiledState =
           newB.state.compile(project, arguments = Some(List("--link")))
-        assert(compiledState.status == ExitStatus.Ok)
+        assertEquals(compiledState.status, ExitStatus.Ok)
+        assert(
+          logger
+            .getMessagesAt(level = Some("info"))
+            .find(_.contains("Generated JavaScript file"))
+            .size == 1
+        )
       }
     }
   }
