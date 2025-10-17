2032511071b740f9eb0e701f2204e23e9e071a5d
Tomasz Godzik <tgodzik@virtuslab.com>
2025-10-17 16:56:10 +0200

    improvement: Don't use absolute path which might be longer than the limit

--- frontend/src/main/scala/bloop/cli/Validate.scala
+++ frontend/src/main/scala/bloop/cli/Validate.scala
@@ -25,7 +25,7 @@ object Validate {
     val cliOptions = cmd.cliOptions
     val commonOptions = cliOptions.common
 
-    def validateSocket = cmd.socket.map(_.normalize.toAbsolutePath) match {
+    def validateSocket = cmd.socket.map(_.normalize) match {
       case Some(socket) if Files.exists(socket) =>
         cliError(Feedback.existingSocketFile(socket), commonOptions)
       case Some(socket) if !Files.exists(socket.getParent) =>
