8b4a93fa69012fd9f174803f55f07455c48ac7ba
Scala Steward <scala_steward@virtuslab.com>
2026-06-08 18:08:57 +0000

    build(deps): Update monix from 3.2.0 to 3.2.2

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -28,7 +28,7 @@ object Dependencies {
   val junitVersion = "0.13.3"
   val jupiterInterfaceVersion = "0.19.0"
   val directoryWatcherVersion = "0.8.0+6-f651bd93"
-  val monixVersion = "3.2.0"
+  val monixVersion = "3.2.2"
   val jsoniterVersion = "2.13.3.2"
   val shapelessVersion = "2.3.4"
   val scalaNative04Version = "0.4.17"
