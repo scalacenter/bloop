addc9bb19abaa3d1cebfe1e093681b006fa0d6ed
Scala Steward <scala_steward@virtuslab.com>
2026-07-18 22:27:58 +0000

    build(deps): Update sbt, scripted-plugin, test-agent from 1.12.13 to 1.12.14

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -24,7 +24,7 @@ object Dependencies {
   val coursierVersion = "2.1.25-M26"
   val sourcecodeVersion = "0.4.4"
   val sbtTestInterfaceVersion = "1.0"
-  val sbtTestAgentVersion = "1.12.13"
+  val sbtTestAgentVersion = "1.12.14"
   val junitVersion = "0.13.3"
   val jupiterInterfaceVersion = "0.19.0"
   val directoryWatcherVersion = "0.8.0+6-f651bd93"
