603df835b84c75c688823d30aa77ac38df6ea614
Scala Steward <scala_steward@virtuslab.com>
2026-09-23 00:32:36 +0000

    build(deps): Update coursier, coursier-jvm from 2.1.25-M26 to 2.1.25

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -21,7 +21,7 @@ object Dependencies {
   val lmVersion = "1.12.3"
   val configDirsVersion = "26"
   val caseAppVersion = "2.0.6"
-  val coursierVersion = "2.1.25-M26"
+  val coursierVersion = "2.1.25"
   val sourcecodeVersion = "0.4.4"
   val sbtTestInterfaceVersion = "1.0"
   val sbtTestAgentVersion = "1.13.0"
